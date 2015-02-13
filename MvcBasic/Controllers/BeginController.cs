fe6d25040b485b8a8a22e5f83e2bd873ca9d19f9
ToshiTomokia <t-akimoto@net-e.co.jp>
2015-02-13 16:34:13 +0900

    new

---
+++ MvcBasic/Controllers/BeginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBasic.Models;
+
+namespace MvcBasic.Controllers
+{
+    public class BeginController : Controller
+    {
+        private MvcBasicContext db = new MvcBasicContext();
+
+        public ActionResult List()
+        {
+            return View(db.Members);
+        }
+
+        // GET: Begin
+        public ActionResult Index()
+        {
+            return Content("こんにちは、世界！");
+        }
+        public ActionResult Show()
+        {
+            ViewBag.Message = "hello world!";
+            return View();
+        }
+    }
+}
---
+++ MvcBasic/Models/MvcBasicInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace MvcBasic.Models
+{
+    public class MvcBasicInitializer : DropCreateDatabaseAlways<MvcBasicContext>
+    {
+        protected override void Seed(MvcBasicContext context)
+        {
+            var members = new List<Member> {
+                new Member {
+                    Name = "yamada rio",
+                    Email = "rio@email.co.jp",
+                    Birth = DateTime.Parse("1980-06-25"),
+                    Married = false,
+                    Memo = "liked piano"
+                },
+                new Member {
+                    Name = "hio naohiro",
+                    Email = "naohiro@email.co.jp",
+                    Birth = DateTime.Parse("1975-07-19"),
+                    Married = false,
+                    Memo = "I am Doctor"
+                }
+            };
+            members.ForEach(m => context.Members.Add(m));
+            context.SaveChanges();
+        }
+    }
+}
